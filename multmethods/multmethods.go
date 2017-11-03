2f3fb9b2106bddebfa239efb3248efbde667614e
Adrian Smith <adrianrsmith93@gmail.com>
2017-11-03 09:41:57 -0700

    FFT and Mult done

--- multmethods/multmethods.go
+++ multmethods/multmethods.go
@@ -5,13 +5,17 @@ import (
 	"math/cmplx"
 )
 
+// FftConv is a function which takes to vectors representing polynomials and
+// returns their convolution.
+// x and y mus tbe the same length and their length must be a multiple of 2
+// This method is not guarunteed to not mutate its input
 func FftConv(x []int, y []int) (res []int) {
 	xFft := make([]complex128, len(x)*2)
 	yFft := make([]complex128, len(x)*2)
 	for i, elem := range x {
 		xFft[i] = complex(float64(elem), float64(0))
 	}
-	for i, elem := range x {
+	for i, elem := range y {
 		yFft[i] = complex(float64(elem), float64(0))
 	}
 
@@ -24,13 +28,23 @@ func FftConv(x []int, y []int) (res []int) {
 	}
 
 	resCmplx := InvFft(convFft)
+	res = make([]int, len(resCmplx))
 	for i, elem := range resCmplx {
-		res[i] = int(real(elem))
+		var roundElem float64
+		if real(elem) < 0 {
+			roundElem = real(elem) - .5
+		} else {
+			roundElem = real(elem) + .5
+		}
+		res[i] = int(roundElem)
 	}
 
 	return
 }
 
+// Fft is a function which produces the Discreet Fourier Transform of input data
+// It will mutate any input passed to it, so if you need to preserve input
+// make sure to deref the slice you pass in.
 func Fft(data []complex128) []complex128 {
 	if len(data) == 1 {
 		return data
@@ -43,6 +57,7 @@ func Fft(data []complex128) []complex128 {
 		odd[i] = (data[i] - data[i+hl]) *
 			cmplx.Exp(complex(0, 2*float64(i)*math.Pi/float64(len(data))))
 	}
+
 	Fft(even)
 	Fft(odd)
 	for i := 0; i < hl; i++ {
@@ -56,21 +71,20 @@ func Fft(data []complex128) []complex128 {
 func InvFft(data []complex128) []complex128 {
 	revStr := data[1:]
 	last := len(revStr) - 1
-	for i := 1; i < len(revStr)/2; i++ {
+	for i := 0; i < len(revStr)/2; i++ {
 		revStr[i], revStr[last-i] = revStr[last-i], revStr[i]
 	}
 
 	Fft(data)
-	scale := 1.0 / float64(len(data))
 	for i := range data {
-		data[i] = complex(imag(data[i])*scale, real(data[i])*scale)
+		data[i] = data[i] / complex(float64(len(data)), float64(0))
 	}
 
 	return data
 }
 
 func DirectMult(x []int, y []int) (res []int) {
-	out := make([]int, len(x) + len(y))
+	out := make([]int, len(x)+len(y))
 	for i := 0; i < len(x); i++ {
 		for j := 0; j < len(y); j++ {
 			out[i+j] += x[i] * y[j]
--- multmethods/multmethods_test.go
+++ multmethods/multmethods_test.go
@@ -1,1 +1,79 @@
 package multmethods
+
+import (
+	"reflect"
+	"testing"
+)
+
+func roundHelper(x complex128) complex128 {
+	real := real(x)
+	cmplx := imag(x)
+	if real < 0 {
+		real = float64(int(real - .5))
+	} else {
+		real = float64(int(real + .5))
+	}
+	if cmplx < 0 {
+		cmplx = float64(int(cmplx - .5))
+	} else {
+		cmplx = float64(int(cmplx + .5))
+	}
+
+	return complex(real, cmplx)
+}
+
+var fftTest = []struct {
+	in  []complex128
+	out []complex128
+}{
+	{[]complex128{complex(5, 0), complex(0, 0)},
+		[]complex128{complex(5, 0), complex(5, 0)}},
+	{[]complex128{complex(1, 0), complex(-1, 0), complex(0, 0), complex(3, 0)},
+		[]complex128{complex(3, 0), complex(1, -4), complex(-1, 0), complex(1, 4)}},
+}
+
+func TestFft(t *testing.T) {
+	for _, tt := range fftTest {
+		actual := Fft(tt.in)
+		for i, elem := range actual {
+			actual[i] = roundHelper(elem)
+		}
+
+		if !(reflect.DeepEqual(actual, tt.out)) {
+			t.Errorf("Fft(%v) => %v, want %v", tt.in, actual, tt.out)
+		}
+	}
+}
+
+var multTests = []struct {
+	inX []int
+	inY []int
+	out []int
+}{
+	{[]int{0}, []int{0}, []int{0}},
+	{[]int{1}, []int{1}, []int{1}},
+	{[]int{0, 1}, []int{0, 1}, []int{0, 0, 1}},
+	{[]int{1, -1, 1, -2}, []int{1, -1, 2, -4}, []int{1, -2, 4, -9, 8, -8, 8}},
+}
+
+func TestFftConv(t *testing.T) {
+	for _, tt := range multTests {
+		actual := FftConv(tt.inX, tt.inY)
+		actual = actual[:len(actual)-1]
+
+		if !(reflect.DeepEqual(actual, tt.out)) {
+			t.Errorf("FftConv(%v, %v) => %v, want %v", tt.inX, tt.inY, actual, tt.out)
+		}
+	}
+}
+
+func TestDirectMult(t *testing.T) {
+	for _, tt := range multTests {
+		actual := DirectMult(tt.inX, tt.inY)
+		actual = actual[:len(actual)-1]
+
+		if !(reflect.DeepEqual(actual, tt.out)) {
+			t.Errorf("FftConv(%v, %v) => %v, want %v", tt.inX, tt.inY, actual, tt.out)
+		}
+	}
+}
